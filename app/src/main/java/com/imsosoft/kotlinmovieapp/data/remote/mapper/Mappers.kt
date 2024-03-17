7e43c8156d8ad58110509d74ff29f7e8822b1c25
Muarem <s.muarem10@gmail.com>
2024-03-17 20:17:36 +0400

    created movies extension func

--- app/src/main/java/com/imsosoft/kotlinmovieapp/data/remote/mapper/Mappers.kt
+++ app/src/main/java/com/imsosoft/kotlinmovieapp/data/remote/mapper/Mappers.kt
@@ -1,6 +1,8 @@
 package com.imsosoft.kotlinmovieapp.data.remote.mapper
 
 import com.imsosoft.kotlinmovieapp.data.remote.dto.MovieDetailDTO
+import com.imsosoft.kotlinmovieapp.data.remote.dto.MoviesDTO
+import com.imsosoft.kotlinmovieapp.domain.model.Movie
 import com.imsosoft.kotlinmovieapp.domain.model.MovieDetail
 
 
@@ -8,4 +10,10 @@ fun MovieDetailDTO.toMovieDetail(): MovieDetail {
     return MovieDetail(
         actors, awards, country, director, genre, language, poster, rated, released, title, type, year, imdbRating
     )
+}
+
+fun MoviesDTO.toMovieList(): List<Movie> {
+    return search.map {
+        search -> Movie(search.poster, search.title, search.year, search.imdbID)
+    }
 }
