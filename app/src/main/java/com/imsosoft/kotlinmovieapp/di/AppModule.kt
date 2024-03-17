00d2d9e3571c9e167196893be518a5df30bed4a9
Muarem <s.muarem10@gmail.com>
2024-03-17 20:44:11 +0400

    created hilt app module

--- app/src/main/java/com/imsosoft/kotlinmovieapp/di/AppModule.kt
+++ app/src/main/java/com/imsosoft/kotlinmovieapp/di/AppModule.kt
@@ -1,10 +1,36 @@
 package com.imsosoft.kotlinmovieapp.di
 
+import com.imsosoft.kotlinmovieapp.data.remote.IMovieService
+import com.imsosoft.kotlinmovieapp.data.repository.MovieRepo
+import com.imsosoft.kotlinmovieapp.domain.repository.IMovieRepo
+import com.imsosoft.kotlinmovieapp.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMovieApi(): IMovieService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(IMovieService::class.java)
+    }
+
+
+
+    @Provides
+    @Singleton
+    fun provideMovieRepo(api: IMovieService): IMovieRepo = MovieRepo(api = api)
+
+
 }
