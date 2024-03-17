9aa7cf6c17a769e067bdf504221f684dbf160208
Muarem <s.muarem10@gmail.com>
2024-03-17 19:47:41 +0400

    created constants

---
+++ app/src/main/java/com/imsosoft/kotlinmovieapp/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.imsosoft.kotlinmovieapp.util
+
+object Constants {
+
+    const val BASE_URL = "http://www.omdbapi.com/"
+    const val API_KEY = ApiKey.MY_API_KEY
+    const val IMDB_ID = "imdb_id"
+    const val POINT = "."
+    const val SEARCH = "s"
+    const val ID = "i"
+    const val KEY = "apikey"
+
+}
