6b19592ea44f71ef5d49707b9602db337efb9031
Muarem <s.muarem10@gmail.com>
2024-03-18 17:22:33 +0400

    created TitleText

--- app/src/main/java/com/imsosoft/kotlinmovieapp/presentation/views/components/Texts.kt
+++ app/src/main/java/com/imsosoft/kotlinmovieapp/presentation/views/components/Texts.kt
@@ -1,5 +1,6 @@
 package com.imsosoft.kotlinmovieapp.presentation.views.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -35,4 +37,18 @@ fun ErrorText(text: String) {
         )
     }
 
+}
+
+@Composable
+fun TitleText(text: String) {
+    Text(
+        text = text,
+        fontFamily = FontFamily.Cursive,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        textAlign = TextAlign.Center,
+        color = Color.White,
+        overflow = TextOverflow.Ellipsis,
+    )
 }
