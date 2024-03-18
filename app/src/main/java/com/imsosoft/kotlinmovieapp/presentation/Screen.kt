77145dea5d767b8f8bfc4d7bcdd85adf2a75b08c
Muarem <s.muarem10@gmail.com>
2024-03-18 07:24:31 +0400

    created Screen.kt

---
+++ app/src/main/java/com/imsosoft/kotlinmovieapp/presentation/Screen.kt
@@ -0,0 +1,6 @@
+package com.imsosoft.kotlinmovieapp.presentation
+
+sealed class Screen(val route: String) {
+    data object MovieScreen: Screen("movie_screen")
+    data object MovieDetailScreen: Screen("movie_detail_screen")
+}
