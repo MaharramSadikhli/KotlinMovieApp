50f38064c63da59e738fdb44218bf145606dd4e3
Muarem <s.muarem10@gmail.com>
2024-03-18 17:22:55 +0400

    created TitleText

--- app/src/main/java/com/imsosoft/kotlinmovieapp/presentation/views/components/Row.kt
+++ app/src/main/java/com/imsosoft/kotlinmovieapp/presentation/views/components/Row.kt
@@ -3,33 +3,37 @@ package com.imsosoft.kotlinmovieapp.presentation.views.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.imsosoft.kotlinmovieapp.domain.model.Movie
 
 @OptIn(ExperimentalCoilApi::class)
 @Composable
 fun MovieListRow(
-    movie: Movie,
-    onItemClick: (Movie) -> Unit
+    movie: Movie, onItemClick: (Movie) -> Unit
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { onItemClick(movie) }
-            .padding(10.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .clickable { onItemClick(movie) }
+        .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween) {
 
         Image(
             painter = rememberAsyncImagePainter(model = movie.poster),
@@ -39,5 +43,16 @@ fun MovieListRow(
                 .size(200.dp, 200.dp)
                 .clip(RectangleShape)
         )
+
+        Column(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            TitleText(text = movie.title)
+            TitleText(text = movie.year)
+
+        }
+
     }
 }
